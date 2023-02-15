f41fc9bfbac45030b933347f32214ade7a912fb5
Cale Duguid <cale.duguid@studytafensw.edu.au>
2023-02-15 14:30:54 +1100

    Initial commit

---
+++ Week 02/conditional_statements_1.py
@@ -0,0 +1,22 @@
+number = 100
+if number > 10:
+    print("the number is bigger than 5")
+else:
+    print("the number is smaller than 5")
+if 10 < 5:
+  print ("False, statement skipped")
+  elif 0 < 5:
+    print ("true, block executed")
+    elif 0 < 3:
+    print ("true, block will also be execute")
+  else:
+    print ("If all fails.")
+  elif 10 < 5:
+      print ("False, this block will not be executed")
+  elif 0 < 5:
+        print ("True, block will be executed")
+  elif 0 < 3:
+    print ("True, this block will also be executed")
+  else:
+    print ("If all fails, executed this")
+
